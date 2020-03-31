680fe8e31c8d07dce4b863bab8f4387f07244fb8
gmeisinger <gmeisinger@gmail.com>
2020-03-31 17:38:12 -0400

    3 hit rocks and animations

--- Scenes/Rock/Rock.gd
+++ Scenes/Rock/Rock.gd
@@ -1,13 +1,31 @@
 extends Node2D
 
 var smashed = false
+var HP = 3
 
 func smash():
-	if not smashed:
-		smashed = true
+	if HP > 0:
+		HP -= 1
 		$Smash.play()
-		#also play animation
+		play_smash_anim()
+		
+
+func play_smash_anim():
+	if HP == 2:
+		play_anim("smash1")
+	elif HP == 1:
+		play_anim("smash2")
+	else:
+		play_anim("destroy")
 
 #change this to the visual animation later
 func _on_Smash_finished():
 	queue_free()
+
+func play_anim(action):
+	if $anim.get_current_animation() != action:
+		$anim.play(action)
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "destroy":
+		queue_free()
